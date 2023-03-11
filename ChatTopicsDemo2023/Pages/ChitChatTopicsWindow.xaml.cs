3e48bdc6f6f69d0b1a56648022b580be2d6913fa
булат шакиров <a2009-1@mail.ru>
2023-03-11 08:52:13 +0300

    add ChitChat and LoginWindow

---
+++ ChatTopicsDemo2023/Pages/ChitChatTopicsWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ChatTopicsDemo2023.db;
+
+namespace ChatTopicsDemo2023.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ChitChatTopicsWindow.xaml
+    /// </summary>
+    public partial class ChitChatTopicsWindow : Window
+    {
+        public ChatDBEntities dbEntities = new ChatDBEntities();
+        Chatroom chatroom;
+        public ChitChatTopicsWindow()
+        {
+            InitializeComponent();
+            NameTB.Text = LoginWindow.employee.Name;
+            var chatRoom = ((Employee)LoginWindow.employee).Id;
+            TopicLV.ItemsSource = dbEntities.ChatMessage.Where(x => x.Id == chatRoom).ToList();
+        }
+
+        private void EmplFinderBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ChitChatWindow chitChat = new ChitChatWindow();
+            chitChat.Show();
+            this.Close();
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void TopicLst_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var top = TopicLV.SelectedItem as ChatMessage;
+            TopicWindow topic = new TopicWindow(top);
+            topic.Show();
+            this.Close();
+
+        }
+    }
+}
--- ChatTopicsDemo2023/Pages/ChitChatWindow.xaml.cs
+++ ChatTopicsDemo2023/Pages/ChitChatWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ChatTopicsDemo2023
+namespace ChatTopicsDemo2023.Pages
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для ChitChatWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ChitChatWindow : Window
     {
-        public MainWindow()
+        public ChitChatWindow()
         {
             InitializeComponent();
         }
